07dbbde79f4023e6e1685470e7b1995078bab802
Shubham6697 <50530172+Shubham6697@users.noreply.github.com>
2020-11-01 14:56:27 +0530

    Update ArmstrongNumber.cpp

--- ArmstrongNumber.cpp
+++ ArmstrongNumber.cpp
@@ -3,25 +3,17 @@ using namespace std;
 
 // armstrong number is a number when we cube the all digit and sum then the same number found.
 
-int main()
-{
-    int n,p,rem,s=0;
-    cin>>n;
-    p=n;
-    while(n)
-    {
-
-        rem=n%10;
-        s=s+rem*rem*rem;
-        n/=10;
+int main(){
+    int num,n1,n2=0;
+    cin>>num;
+    n1=num;
+    while(n1!=0){
+        n2+=(n1%10)*(n1%10)*(n1%10);
+        n1/=10;
     }
-    if(s==p)
-    {
+    if(n2 == num)
         cout<<"Armstrong Number";
-    }
-    else{
+    else
         cout<<"Not a Armstrong number";
-    }
     return 0;
-
 }
